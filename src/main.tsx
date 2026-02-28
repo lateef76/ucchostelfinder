72db96e2429996e2d244f9bbbd39531c67a26325
lateef <lateef@gmail.com>
2026-02-28 00:55:12 +0000

    mobile optimized and tailwindcss setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Detect if user is on mobile for any initial setup
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+// Set viewport height fix for mobile browsers (address bar issues)
+const setVhVariable = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+// Initial set
+setVhVariable();
+
+// Reset on resize
+window.addEventListener("resize", setVhVariable);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
